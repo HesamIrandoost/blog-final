b4dcd6f12212148ce0eac7a05471601ce74cecf1
hesam <hesamirandoost14@gmail.com>
2026-06-08 18:41:17 +0330

    Comment: add comment model

--- Comment/models.py
+++ Comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Blog.models import Post
+
 
 # Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=250)
+    status =models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
