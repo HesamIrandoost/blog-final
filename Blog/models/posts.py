9a9d728dcd1b54fc1a96be86b0418286839f48b1
hesam <hesamirandoost14@gmail.com>
2026-06-08 18:40:24 +0330

    Blog: add Post and Category model

---
+++ Blog/models/posts.py
@@ -0,0 +1,28 @@
+from django.db import models
+from Accounts.models import Profile
+import uuid
+from django.utils.text import slugify
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    slug = models.SlugField(max_length=60, unique=True, blank=True, editable=False)
+    title = models.CharField(max_length=250)
+    cover = models.ImageField(upload_to=("upload/cover/"), blank=True, null=True)
+    content = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title[:50])
+            self.slug = f"{base_slug}-{uuid.uuid4().hex[:8]}"
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title[:20]
